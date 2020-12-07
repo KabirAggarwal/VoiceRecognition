a92b505e6f868534bf6911e8c974ed8561f821b7
Kabir Aggarwal <55024992+kabira789@users.noreply.github.com>
2020-12-08 00:45:30 +0530

    Completed The project

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,27 +1,30 @@
 "use strict";
 
-var msgEl = document.getElementById("msg");
+var msgEl = document.getElementById("msg"); //Get Random number
 
 function getRandomNumber() {
   return Math.floor(Math.random() * 100) + 1;
 }
 
 var rand_num = getRandomNumber();
-console.log("number : ", rand_num);
+console.log("number : ", rand_num); //use of recoginition services
+
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-var recognition = new window.SpeechRecognition();
-recognition.start();
+var recognition = new window.SpeechRecognition(); //method to start voice recog...
+
+recognition.start(); // taking the words of the user
 
 function onspeak(e) {
-  var msg = e.results[0][0].transcript; //console.log(msg);
-
+  var msg = e.results[0][0].transcript;
   display_message(msg);
   number_verify(msg);
-}
+} //Msg display
+
 
 function display_message(msg) {
   msgEl.innerHTML = "\n    <div>Your Words : </div>\n    <span class=\"box\">".concat(msg, "</span>\n    ");
-}
+} //The main code and verifying the user input
+
 
 function number_verify(msg) {
   var num = +msg;
@@ -37,18 +40,21 @@ function number_verify(msg) {
   }
 
   if (num === rand_num) {
-    document.body.innerHTML = "\n        <h2>Yippee!!! Correct number guessed .. <br><br>\n        Number was : ".concat(num, " </h2>\n        <button class=\"play-again\" id=\"play again\">Play Again</button>");
+    document.body.innerHTML = "\n        <h2>Yippee!!! Correct number guessed .. <br><br>\n        Number was : ".concat(num, " </h2>\n        <button class=\"play-again\" id=\"play-again\">Play Again</button>");
   } else if (num > rand_num) {
     msgEl.innerHTML += "<div>Go Lower</div>";
   } else {
     msgEl.innerHTML += "<div>Go Higher</div>";
   }
-}
+} //capturing the result
 
-recognition.addEventListener('result', onspeak);
+
+recognition.addEventListener('result', onspeak); //recalling the function uptill correct answer
+
 recognition.addEventListener('end', function () {
   return recognition.start();
-});
+}); //For playing again the game
+
 document.body.addEventListener('click', function (e) {
   if (e.target.id == 'play-again') {
     window.location.reload();
--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 const msgEl = document.getElementById("msg");
 
-
+//Get Random number
 function getRandomNumber(){
     return Math.floor(Math.random() * 100 ) + 1
 }
+
 const rand_num = getRandomNumber();
 console.log("number : ",rand_num)
 
+
+//use of recoginition services
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 let recognition = new window.SpeechRecognition();
 
+//method to start voice recog...
 recognition.start();
 
+// taking the words of the user
 function onspeak(e){
     const msg = e.results[0][0].transcript;
-
-    //console.log(msg);
     display_message(msg)
     number_verify(msg)
 }
 
+//Msg display
 function display_message(msg){
     msgEl.innerHTML =`
     <div>Your Words : </div>
     <span class="box">${msg}</span>
     `
 }
-
+//The main code and verifying the user input
 function number_verify(msg){
     const num = +msg;
 
@@ -43,7 +47,7 @@ function number_verify(msg){
         document.body.innerHTML =`
         <h2>Yippee!!! Correct number guessed .. <br><br>
         Number was : ${num} </h2>
-        <button class="play-again" id="play again">Play Again</button>`
+        <button class="play-again" id="play-again">Play Again</button>`
     }
     else if (num >rand_num){
         msgEl.innerHTML +=`<div>Go Lower</div>`
@@ -53,10 +57,14 @@ function number_verify(msg){
     }
 }
 
+//capturing the result
 recognition.addEventListener('result', onspeak); 
 
+//recalling the function uptill correct answer
 recognition.addEventListener('end', () => recognition.start())
 
+
+//For playing again the game
 document.body.addEventListener('click', e  => {
     if(e.target.id == 'play-again'){
         window.location.reload();
