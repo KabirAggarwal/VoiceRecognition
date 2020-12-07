b357cecd2cf2df5e648251f20e4e13f37a0ad544
Kabir Aggarwal <55024992+kabira789@users.noreply.github.com>
2020-12-07 22:07:11 +0530

    change2

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -7,3 +7,19 @@ function getRandomNumber() {
 }
 
 var rand_num = getRandomNumber();
+console.log("number : ", rand_num);
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+var recognition = new window.SpeechRecognition();
+recognition.start();
+
+function onspeak(e) {
+  var msg = e.results[0][0].transcript; //console.log(msg);
+
+  display_message(msg); // number_verify(msg)
+}
+
+function display_message(msg) {
+  msgEl.innerHTML = "\n    <div>Your Words : </div>\n    <span class=\"box\">".concat(msg, "</span>\n    ");
+}
+
+recognition.addEventListener('result', onspeak);
--- script.js
+++ script.js
@@ -4,8 +4,29 @@ const msgEl = document.getElementById("msg");
 function getRandomNumber(){
     return Math.floor(Math.random() * 100 ) + 1
 }
-
-
 const rand_num = getRandomNumber();
+console.log("number : ",rand_num)
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+
+let recognition = new window.SpeechRecognition();
+
+recognition.start();
+
+function onspeak(e){
+    const msg = e.results[0][0].transcript;
+
+    //console.log(msg);
+    display_message(msg)
+   // number_verify(msg)
+}
+
+function display_message(msg){
+    msgEl.innerHTML =`
+    <div>Your Words : </div>
+    <span class="box">${msg}</span>
+    `
+}
 
 
+recognition.addEventListener('result',onspeak); 
