780339e0ae09f4c86da331215e1196ab63aa92d7
Kabir Aggarwal <55024992+kabira789@users.noreply.github.com>
2020-12-07 14:58:23 +0530

    changes

---
+++ dist/script.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var msgEl = document.getElementById("msg");
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+var rand_num = getRandomNumber();
---
+++ script.js
@@ -0,0 +1,11 @@
+const msgEl = document.getElementById("msg");
+
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * 100 ) + 1
+}
+
+
+const rand_num = getRandomNumber();
+
+
